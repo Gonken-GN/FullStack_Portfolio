4d6758132365776b1ab7dd8f1376cee819ac3650
Fadhil <fadhilanugrah21@gmail.com>
2023-08-10 22:49:40 +0700

    add minor function

--- frontend/src/container/Testimonials/Testimonials.jsx
+++ frontend/src/container/Testimonials/Testimonials.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { AppWrap,MotionWrap } from "../../Wrapper";
+import { urlFor, client } from "../../client";
 
 import './Testimonials.scss';
 const Testimonials = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState([]);
+
   return (
     <div>Testimonials</div>
   )
